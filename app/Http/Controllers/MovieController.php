2d500af4b9b302216a2d2a01a91f7efcd23129c0
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-03-11 14:57:51 +0100

    add model

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class MovieController extends Controller
 {
     public function index(){
-        $data=Movie::all();
+        $data=Movie::  all();
         dump($data);
 
         return view('home');
