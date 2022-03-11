060a58ffa78f5efa7b115aa838595ce28259e05f
ManuelAvanzi <manuel.92.avanzi@gmail.com>
2022-03-11 14:56:29 +0100

    origin

---
+++ resources/views/home.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.default')
+
+@section('pageTitle')
+ Laravel Movies
+@endsection
+
+@section('content')
+
+<main>
+    <div class="cards-container">
+
+    <h2>qui andrà il fim</h2>
+            
+       
+@endsection
+
+    </div>
+
+
+</main>
+ 
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,7 @@
+<header>
+    <div class="header-container">
+
+      
+      
+    </div>
+</header>
